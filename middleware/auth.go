d2e50c423e661959470334ce5fe69d88d2453711
Igor Belykh <dedulyxx@yandex.ru>
2025-06-26 18:57:31 +0300

    add auth middleware

---
+++ middleware/auth.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func IsAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authorization := r.Header.Get("Authorization")
+		if authorization == "" {
+			fmt.Println("Is not auth")
+			return
+		}
+		auth := strings.TrimPrefix(authorization, "Bareer ")
+		fmt.Println(auth)
+		next.ServeHTTP(w, r)
+	})
+}
